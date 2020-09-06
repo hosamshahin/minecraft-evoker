e8026837a1818730763c5f4229ae288875754f1e
hshahin <hshahin@vt.edu>
2020-09-06 15:41:52 -0400

    convert template to yaml

--- helpers/create-stack.sh
+++ helpers/create-stack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 DIRECTORY="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
 DEFAULT_STACK_NAME=minecraft-stack
-aws cloudformation create-stack --stack-name ${STACK_NAME-DEFAULT_STACK_NAME} --template-body file://${DIRECTORY}/cfn/s3.template.json --parameters file://${DIRECTORY}/parameters.json --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name ${DEFAULT_STACK_NAME} --template-body file://${DIRECTORY}/cfn/s3.template.yaml --parameters file://${DIRECTORY}/parameters.json --capabilities CAPABILITY_IAM --profile personalProfile
